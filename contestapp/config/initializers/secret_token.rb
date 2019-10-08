5f17d276c782441d044a535820d969803eb30dac
Joe Strahm <jstrahm@verabradley.com>
2019-10-08 12:19:55 -0400

    First Commit

---
+++ contestapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contestapp::Application.config.secret_key_base = '39b038cda47658471eae7d5421ec93297a4a07aadab5b22fa4ca977fef8e6dddab913186cf84419dac2b3197d128149af8beb66c15b1e1fe0d652a4ed979b9ff'
